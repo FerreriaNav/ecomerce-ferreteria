716f79ccb840cfc1238e7e8a8a30ac18ac75ab91
Eric Carballo <ericjasiel.11@gmail.com>
2025-06-13 00:17:20 -0600

    loading direcciones

--- src/app/(user)/carrito/page.tsx
+++ src/app/(user)/carrito/page.tsx
@@ -1,33 +1,31 @@
-"use client";
-import { BasketGrid } from "@/modules/cart/basket";
-import { Button } from "@/components/ui/button";
-import { ModalAuth } from "@/modules/common/components/auth/modalAuth";
-import { getUserDirections } from "@/services/directions/directions-services";
-import Link from "next/link";
-import { useSession } from "next-auth/react";
-import { useInfoEcommerceStore } from "@/store/info-ecommerce.store";
-import { useEffect, useState } from "react";
-import { DataResponse } from "@/interfaces/data/response.interface";
-import { Address } from "@/interfaces/directions/directions.interface";
+"use client"
+import { BasketGrid } from "@/modules/cart/basket"
+import { Button } from "@/components/ui/button"
+import { ModalAuth } from "@/modules/common/components/auth/modalAuth"
+import { getUserDirections } from "@/services/directions/directions-services"
+import Link from "next/link"
+import { useSession } from "next-auth/react"
+import { useInfoEcommerceStore } from "@/store/info-ecommerce.store"
+import { useEffect, useState } from "react"
+import type { DataResponse } from "@/interfaces/data/response.interface"
+import type { Address } from "@/interfaces/directions/directions.interface"
+import { Loader2 } from "lucide-react"
 
 export default function CartPage() {
+  const { data: session } = useSession()
+  const { infoEcommerce } = useInfoEcommerceStore()
 
-  const { data: session } = useSession();
-  const { infoEcommerce } = useInfoEcommerceStore();
+  const [address, setAddress] = useState<DataResponse<Address[]> | null>(null)
 
-  const [address, setAddress] = useState<DataResponse<Address[]> | null>(null);
-
-  
   useEffect(() => {
     const fetchAddresses = async () => {
       if (session?.user?.user.documentId) {
-        const res = await getUserDirections(session.user.user.documentId);
-        setAddress(res);
+        const res = await getUserDirections(session.user.user.documentId)
+        setAddress(res)
       }
-    };
-    fetchAddresses();
-  }, [session?.user?.user.documentId]);
-
+    }
+    fetchAddresses()
+  }, [session?.user?.user.documentId])
 
   if (!session?.user?.user.documentId) {
     return (
@@ -35,23 +33,28 @@ export default function CartPage() {
         <div className="flex flex-col items-center justify-center min-h-[70vh] text-center">
           <h1 className="text-4xl font-bold mb-6">{`Bienvenido a ${infoEcommerce?.nombre}`}</h1>
           <p className="text-xl mb-8 max-w-md">
-            Descubre asombrosos productos a excelentes precios. Comienza a
-            comprar ahora!
+            Descubre asombrosos productos a excelentes precios. Comienza a comprar ahora!
           </p>
           <div className="flex gap-4">
             <ModalAuth />
             <Button variant="outline" size="lg">
-              <Link href="/">Buscar Products</Link>
+              <Link href="/">Buscar Productos</Link>
             </Button>
           </div>
         </div>
       </div>
-    );
+    )
   }
-
 
   if (!address) {
-    return <div>Cargando direcciones...</div>;
+    return (
+      <div className="flex items-center justify-center min-h-[70vh]">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 animate-spin text-primary" />
+        </div>
+      </div>
+    )
   }
-  return <BasketGrid session={session} addresses={address.data} />;
+
+  return <BasketGrid session={session} addresses={address.data} />
 }
