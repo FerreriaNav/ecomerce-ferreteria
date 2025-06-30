aa61c7eaa45f8edac72077014749fa7ad5a5d868
Lu1sEnriquez <luis.lavz.2300@gmail.com>
2025-06-29 21:56:29 -0700

    config dominios

--- next.config.ts
+++ next.config.ts
@@ -10,8 +10,19 @@ const nextConfig: NextConfig = {
         pathname: "/uploads/**",
       },
       {
+        protocol: "http",
+        hostname: "162.240.102.107",
+        port: "3001",
+        pathname: "/uploads/**",
+      },
+      {
         protocol: "https",
-        hostname: "3d35-2806-2f0-2221-fe88-5125-3f78-944a-f5c7.ngrok-free.app",
+        hostname: "api.dinav.com.mx",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "api.dinav.com.mx",
         pathname: "/uploads/**",
       },
       {
